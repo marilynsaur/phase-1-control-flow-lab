04eeef95d04b08c06852427b8cd39f9d22d5b7ab
Marilyn Wren <Marilynwren64@yahoo.com>
2022-02-01 16:16:34 -0500

    commit tree 4702390ac3f0cfe34104be36d3eacdbb8b446e69

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+
+
+function scuberGreetingForFeet(distance) {
+
+  if (distance <= 199) {
+    return 'This one is on me!'
+  } else if (distance >= 200 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance > 2500) {
+    return 'No can do.'
+  } 
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(NYC) {
   // Write your code here!
+
+  return NYC == 'NYC' ? "Ok, sounds good." : "No go.";
+  
+   
+
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+
+    case 'not as generous':
+      return 'Thank you.';
+
+    case 'thanks for everything':
+      return 'Bye.';
+
+
+  }
+
 }
+
